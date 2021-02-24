687e80a9eacab03c9746c4beb51c4be1c47631d0
pro100user <51286168+pro100user15@users.noreply.github.com>
2021-02-24 11:48:53 +0200

    pro100user
    first commit

--- Лабараторна робота 1/src/Main.java
+++ Лабараторна робота 1/src/Main.java
@@ -1,6 +1,8 @@
 import java.util.Random;
 import java.util.Scanner;
 
+//@author pro100user
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
