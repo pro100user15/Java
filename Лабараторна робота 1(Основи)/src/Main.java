a66085989dcaa2598abcfc9ef90bfa0d8fc28eef
pro100user <51286168+pro100user15@users.noreply.github.com>
2021-02-25 13:25:36 +0200

    pro100user

--- Лабараторна робота 1(Основи)/src/Main.java
+++ Лабараторна робота 1(Основи)/src/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 //@author pro100user
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("1. Перше завдання.");
